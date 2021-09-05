7436c0bf68763ac78f4b9fb3a2f75081587eb876
Jasmine Ryon <jryon12@priorypride.net>
2021-09-04 17:10:50 -0700

    songs controller has new and create actions and songs_params, added create to songs resources

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::SongsController < ApplicationController
+
+    def index
+        songs = Song.all
+        render json: songs
+    end
+
+    def create
+        song = Song.new(song_params)
+        if song.save
+            render json: song, status: :accepted
+        else
+            render json: {errors: song.errors.full_messages}, status: :unprocessible_entity
+            
+        end
+    end
+
+    private
+
+    def song_params
+        params.require(:song).permit(:title, :artist, :img_url, :link, :genre_id)
+    end
+
 end
