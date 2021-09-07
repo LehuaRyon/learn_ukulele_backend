2d2f9463075317d1ed29be7872ac3d63570a991e
Jasmine Ryon <jryon12@priorypride.net>
2021-09-07 14:50:24 -0700

    fixed genre index action

--- app/controllers/api/v1/genres_controller.rb
+++ app/controllers/api/v1/genres_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::GenresController < ApplicationController
 
     def index
         genres = Genre.all
-        render json: SongSerializer.new(genres)
+        render json: GenreSerializer.new(genres)
     end
 
+    # private
+
+    # def genre_params
+    #     params.require(:genre).permit(:name)
+    # end
 end
