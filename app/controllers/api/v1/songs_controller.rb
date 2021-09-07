13b2195da82aac63008f5779083300a21c39c965
Jasmine Ryon <jryon12@priorypride.net>
2021-09-07 16:19:35 -0700

    added destroy message

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -18,7 +18,7 @@ class Api::V1::SongsController < ApplicationController
     def destroy
         song = Song.find_by_id(params[:id])
         song.destroy
-        render json: {message: 'Song successfully deleted!'}
+        render json: {message: "Song successfully deleted! #{song.title}"}
     end
 
     private
